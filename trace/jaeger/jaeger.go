7dd37f88d8cbc48a623a9ad4895810f15aaa52b3
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2017-10-05 16:38:58 +0100

    Adding tracing

---
+++ trace/jaeger/jaeger.go
@@ -0,0 +1,49 @@
+/*
+Package datadog reports metrics to datadog, if included.
+
+  package main
+
+  import(
+    _ "github.com/bsm/rucksack/trace/jaeger"
+  )
+
+  func main() {
+    sleep := make(chan struct{})
+    <-sleep
+  }
+
+Run with:
+
+  TRACE_NAME=myapp TRACE_JAEGER=10.0.0.1 go run main.go
+
+*/
+package jaeger
+
+import (
+	"io"
+	"os"
+	"runtime"
+
+	"github.com/bsm/rucksack/log"
+	"github.com/uber/jaeger-client-go/config"
+)
+
+func init() {
+	if name, host := os.Getenv("TRACE_NAME"), os.Getenv("TRACE_JAEGER"); name != "" && host != "" {
+		cfg := config.Configuration{
+			Sampler: &config.SamplerConfig{
+				SamplingServerURL: "http://" + host + ":5778/sampling",
+			},
+			Reporter: &config.ReporterConfig{
+				LocalAgentHostPort: host + ":5775",
+			},
+		}
+
+		closer, err := cfg.InitGlobalTracer(name)
+		if err != nil {
+			log.Errorf("trace/jaeger: could not initialize tracer: %v", err)
+			return
+		}
+		runtime.SetFinalizer(closer, func(c io.Closer) { _ = c.Close() })
+	}
+}
