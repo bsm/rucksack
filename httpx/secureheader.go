4bd33c6d9729fdc7f422ff135bb68808c4a102c2
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2018-10-01 09:36:22 +0100

    Basic httpx defaults

---
+++ httpx/secureheader.go
@@ -0,0 +1,30 @@
+package httpx
+
+import (
+	"net/http"
+
+	"github.com/bsm/rucksack"
+	"github.com/kr/secureheader"
+)
+
+var secureheaderDefaults secureheader.Config
+
+func init() {
+	secureheaderDefaults = *secureheader.DefaultConfig
+
+	if s := rucksack.Env("SECURE_HEADER_HTTPS_REDIRECT"); s == "false" {
+		secureheaderDefaults.HTTPSRedirect = false
+	}
+	if s := rucksack.Env("SECURE_HEADER_HTTPS_USE_FORWARDED_PROTO"); s != "false" {
+		secureheaderDefaults.HTTPSUseForwardedProto = true
+	}
+}
+
+func secureHeader(cfg *secureheader.Config) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		c := new(secureheader.Config)
+		*c = *cfg
+		c.Next = h
+		return c
+	}
+}
